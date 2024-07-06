556b0a7947705482bd15637a803b34e0c0dd359d
WNomunomu <kohsei.nmr@gmail.com>
2024-07-06 14:48:48 +0000

    generate Micropost model

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_06_142505) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_06_144756) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
